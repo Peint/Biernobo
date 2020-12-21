5b6736477347fca66fa9d78f4d858822f4e8f1e3
Peint <themrcrazyfu@gmail.com>
2020-12-21 10:19:29 +0100

    add Buttons add AppIcon add Text Randomizer

--- app/src/main/java/de/tryhard/biernobo/MainActivity.java
+++ app/src/main/java/de/tryhard/biernobo/MainActivity.java
@@ -1,6 +1,9 @@
 package de.tryhard.biernobo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,6 +14,10 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+    Button clickMeBtn;
+    TextView dynamicText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,19 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+        clickMeBtn = (Button) findViewById(R.id.weiter);
+        dynamicText = (TextView) findViewById(R.id.tasks);
+
+        clickMeBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                count++;
+                dynamicText.setText("dynamic text example : " + count);
+
+            }
+        });
+    }
 }
--- app/src/main/java/de/tryhard/biernobo/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/de/tryhard/biernobo/ui/dashboard/DashboardFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import de.tryhard.biernobo.R;
 
 public class DashboardFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
+        View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        // Inflate the layout for this fragment
+        Button myButton = (Button) rootView.findViewById(R.id.weiter);
+        return rootView;
+
+
+    }
 }
