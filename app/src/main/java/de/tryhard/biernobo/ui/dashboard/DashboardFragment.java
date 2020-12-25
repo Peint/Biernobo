bfca1df167c86ddd089f8bf4806ccb69dc5baa21
Peint <themrcrazyfu@gmail.com>
2020-12-25 20:38:43 +0100

    finalize buttons restructure game tasks added taskarray

--- app/src/main/java/de/tryhard/biernobo/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/de/tryhard/biernobo/ui/dashboard/DashboardFragment.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,16 +26,57 @@ import de.tryhard.biernobo.R;
 public class DashboardFragment extends Fragment {
 
     private String[] tasks = {
-            "trinke 1",
-            "trinke 2",
-            "trinke 3",
-            "trinke 4",
-            "trinke 5",
-            "trinke 6",
-            "trinke 7",
-            "trinke 8",
-            "trinke 9",
-            "trinke 10"
+            "Trinke einen Schluck.",
+            "Trinke 2 Schlucke.",
+            "Trinke 3 Schlucke.",
+            "Trinke 5 Schlucke.",
+            "Trinke 10 Schlucke.",
+            "Verteile einen Schluck.",
+            "Verteile 2 Schlucke.",
+            "Verteile 3 Schlucke.",
+            "Verteile 5 Schlucke.",
+            "Verteile 10 Schlucke.",
+            "Exe dein Getränk.",
+            "Warheit oder Pflicht.",
+            "Ich hab noch nie.",
+            "Sage den Zungenbrecher: 'Zehn zahme Ziegen zogen zehn Zentner Zucker zum Zoo' auf, schaffst du es nicht trinke 5 Schlucke.",
+            "Alle trinken für jeden Buchstaben in seinem Namen",
+            "Jeder der schon einmal ein Traum mit einem Mitspieler hatte trinkt.",
+            "Die Person die als letztes Geburtstag hatte trinkt.",
+            "Alle die einen Bruder haben trinken.",
+            "Alle Männer trinken.",
+            "Alle Frauen trinken.",
+            "Wer schon einmal jemand aus dieser Runde geküsst hat muss trinken.",
+            "Schließe deine Augen und sag welche Farbe das Oberteil von links hat. Falsch.? Dann trink.",
+            "Alle die eine Schwester haben trinken.",
+            "Alle deren Hausnummer gerade ist trinken.",
+            "Du darfst die Mitspieler nur noch mit Nachnamen ansprechen.",
+            "Die nächste Person die du berührst muss trinken.",
+            "Wähle einen Trinkbuddy für 5 Runden.",
+            "Jeder mit schwarzen Socken trinkt.",
+            "Alle deren Handy auf dem Tisch liegt muss trinken.",
+            "Alle die keine Jeans tragen müssen trinken.",
+            "Alle die schon Mal geklaut haben trinken.",
+            "Trink wenn du schon Mal schwarz gefahren bist.",
+            "Trink wenn du schon Mal schwarz gefahren bist.",
+            "Der größte und der kleinste trinken.",
+            "Verteile für jeden Buchstaben in deinem Namen.",
+            "Alle die schon Mal nackt schwimmen waren trinken.",
+            "Trink soviele Schlucke wie du willst, rechts trinkt das doppelte.",
+            "Alle die einen \"A\" im Vorname haben trinken.",
+            "Warst du schon Mal betrunken in der Schule/Uni wenn ja trink.",
+            "Alle die Bock auf Saufen haben stoßen an und nehmen einen kräftigen Schluck.",
+            "Alle die ein \"N\" im Nachnamen haben trinken.",
+            "Weil wir Freunde sind trinken wir alle ein.",
+            "Alle die noch nicht auf Klo waren trinken.",
+            "Alle Raucher trinken 1, alle Nichtraucher 3.",
+            "Hättest du schon einmal ein Tinder (ect.) Date. Dann trink.",
+            "Suche dir jemand aus der jetzt trinkt.",
+            "Alle die Sex schonmal außerhalb vom Bett hätten trinken.",
+            "Trink wenn du schon Mal Karaoke gesungen haben trinken.",
+            "Stelle eine Frage in die Runde, wer zuerst antworte verteilt.",
+            "Alle mit Abi trinken.",
+            "Trink wenn du nicht alle Star wars Filme gesehen hast."
     };
 
     public Integer[] taskHistory = {};
@@ -57,6 +100,11 @@ public class DashboardFragment extends Fragment {
                 taskPanel.setText(tasks[taskHistory[taskHistory.length - 1]]);
             }
         });
+
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(), R.array.PlayerSpinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         return rootView;
     }
